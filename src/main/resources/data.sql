e6718da033b106c4f2cc40c30ae8116351d8f543
OzgurKurdoglu <107494704+OzgurKurdoglu@users.noreply.github.com>
2025-02-27 13:10:35 +0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values (10001,'USD','EUR',2.00,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values (10002,'USD','TRY',40.00,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values (10003,'TRY','EUR',42.00,'');
